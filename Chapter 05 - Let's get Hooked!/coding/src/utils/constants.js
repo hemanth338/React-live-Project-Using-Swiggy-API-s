c3685af662da06b114b1b5058e9b35ce3d40b124
GONA HEMANTH REDDY <147100138+hemanth338@users.noreply.github.com>
2025-09-01 21:28:06 +0530

    Add files via upload

---
+++ Chapter 05 - Let's get Hooked!/coding/src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
+export const LOGO_URL =
+  "https://res.cloudinary.com/dkpst40y3/image/upload/v1756722141/ChatGPT_Image_Aug_22_2025_12_35_39_PM_mavje3.png";
