a07258f7c6af4899a48ba5c1c2d65662283f5d19
GONA HEMANTH REDDY <147100138+hemanth338@users.noreply.github.com>
2025-09-11 13:19:35 +0530

    Add files via upload

---
+++ Chapter 08 - Let's get Classy/Coding/src/utils/constants.js
@@ -0,0 +1,8 @@
+export const CDN_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
+
+export const LOGO_URL =
+  "https://res.cloudinary.com/dkpst40y3/image/upload/v1757053114/Gona_s_Food_Villa_tzuafb.png";
+
+export const MENU_API =
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=16.30070&lng=80.46390&restaurantId=";
